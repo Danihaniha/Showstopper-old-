1caa9952a3d12881cb6fa4cff844b6c2b41db355
Daniel Hansen <da96ha@gmail.com>
2021-03-31 19:43:19 +0200

    Made Journal Button Work

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -6,16 +6,27 @@ public class SettingsMenu : MonoBehaviour
 {
     public GameObject settingsMenuUI;
     public GameObject gameOverlayUI;
+    public GameObject journalUI;
+
 
     public void ResumeGame()
     {
         settingsMenuUI.SetActive(false);
+        journalUI.SetActive(false);
         gameOverlayUI.SetActive(true);
     }
 
     public void SettingsPage()
     {
         settingsMenuUI.SetActive(true);
+        journalUI.SetActive(false);
+        gameOverlayUI.SetActive(false);
+    }
+
+    public void JournalPage()
+    {
+        journalUI.SetActive(true);
+        settingsMenuUI.SetActive(false);
         gameOverlayUI.SetActive(false);
     }
 }
